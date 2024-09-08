dc68141310dbde73561bc40c45c5dcc616e035eb
Daun Kim <yennakim11@gmail.com>
2024-09-08 01:25:37 -0500

    added CRUD API calls for boarding pass

---
+++ utils/data/boardingPassData.js
@@ -0,0 +1,65 @@
+import { clientCredentials } from '../client';
+
+const getBoardingPasses = (uid) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/boarding_pass`, {
+    method: 'GET',
+    headers: {
+      Authorization: uid,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const getSingleBoardingPass = (id) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/boarding_pass/${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const createBoardingPass = (payload) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/boarding_pass`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const updateBoardingPass = (payload) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/boarding_pass/${payload.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then(() => resolve())
+    .catch(reject);
+});
+
+const deleteBoardingPass = (id) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/boarding_pass/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+export {
+  getBoardingPasses, getSingleBoardingPass, createBoardingPass, updateBoardingPass, deleteBoardingPass,
+};
