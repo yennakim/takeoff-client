e88d1de305a9a627e47e7f54ebd04956e6a70360
Daun Kim <yennakim11@gmail.com>
2024-09-08 01:19:47 -0500

    added CRUD API calls for lodging

---
+++ utils/data/lodgingData.js
@@ -0,0 +1,65 @@
+import { clientCredentials } from '../client';
+
+const getLodgings = (uid) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/lodging`, {
+    method: 'GET',
+    headers: {
+      Authorization: uid,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const getSingleLodging = (id) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/lodging/${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const createLodging = (payload) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/lodging`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const updateLodging = (payload) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/lodging/${payload.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then(() => resolve())
+    .catch(reject);
+});
+
+const deleteLodging = (id) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/lodging/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+export {
+  getLodgings, getSingleLodging, createLodging, updateLodging, deleteLodging,
+};
