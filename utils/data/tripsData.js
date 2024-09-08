f0b962710c1909982721c8100d64b34ff9ff9e99
Daun Kim <yennakim11@gmail.com>
2024-09-08 01:15:45 -0500

    added create, update, and delete API calls for trips

--- utils/data/tripsData.js
+++ utils/data/tripsData.js
@@ -24,4 +24,42 @@ const getSingleTrip = (id) => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
-export { getTrips, getSingleTrip };
+const createTrip = (payload) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/trips`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const updateTrip = (payload) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/trips/${payload.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then(() => resolve())
+    .catch(reject);
+});
+
+const deleteTrip = (id) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/trips/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+export {
+  getTrips, getSingleTrip, createTrip, updateTrip, deleteTrip,
+};
