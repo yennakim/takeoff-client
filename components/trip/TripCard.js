ec0af3bf53b7e74648ac841961b90c69a58a63f8
Daun Kim <yennakim11@gmail.com>
2024-09-08 17:59:10 -0500

    created trip card component and have it rendering on the Trips page '/trips'

---
+++ components/trip/TripCard.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, Card } from 'react-bootstrap';
+import Link from 'next/link';
+import { deleteTrip } from '../../utils/data/tripsData';
+import { useAuth } from '../../utils/context/authContext';
+
+export default function TripCard({ tripObj, onUpdate }) {
+  const { user } = useAuth();
+  const deleteThisTrip = () => {
+    if (window.confirm(`Delete ${tripObj.trip_name}?`)) {
+      deleteTrip(tripObj.id).then(() => onUpdate());
+    }
+  };
+  return (
+    <Card className="card" style={{ width: '18rem', margin: '10px', border: '1px solid' }}>
+      <Card.Title style={{ textAlign: 'center', paddingTop: '10px' }}>{tripObj.trip_name}</Card.Title>
+      <Card.Body>
+        {/* <Card.Img variant="top" src={tripObj.image} alt={tripObj.trip_name} style={{ height: '400px', borderRadius: '0.5rem' }} /> */}
+        <p style={{ textAlign: 'center' }} className="card-text bold">{tripObj.start_date} - {tripObj.end_date}</p>
+        <Link Link href={`/view/${tripObj.id}`} passHref>
+          <Button variant="outline-success" className="m-2">VIEW</Button>
+        </Link>
+        {user.uid === tripObj.uid && (
+        <>
+          <Link href={`/trip/edit/${tripObj.id}`} passHref>
+            <Button variant="info">EDIT</Button>
+          </Link>
+          <Button variant="danger" onClick={deleteThisTrip} className="m-2">
+            DELETE
+          </Button>
+        </>
+        )}
+      </Card.Body>
+    </Card>
+  );
+}
+
+TripCard.propTypes = {
+  tripObj: PropTypes.shape({
+    id: PropTypes.number,
+    // image: PropTypes.string,
+    trip_name: PropTypes.string,
+    start_date: PropTypes.string,
+    end_date: PropTypes.string,
+    uid: PropTypes.string,
+  }).isRequired,
+  onUpdate: PropTypes.func.isRequired,
+};
---
+++ pages/trips.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { Button } from 'react-bootstrap';
+import { useAuth } from '../utils/context/authContext';
+import TripCard from '../components/trip/TripCard';
+import { getTrips } from '../utils/data/tripsData';
+
+export default function TripsPage() {
+  const [trips, setTrips] = useState([]);
+  const { user } = useAuth();
+  const getUserTrips = () => {
+    getTrips(user.uid).then(setTrips);
+  };
+
+  useEffect(() => {
+    getUserTrips();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user]);
+
+  return (
+    <div className="text-center my-4">
+      <Link href="/trip/addTrip" passHref>
+        <Button variant="success">Add Trip</Button>
+      </Link>
+      <div className="d-flex flex-wrap">
+        {trips.map((trip) => (
+          <TripCard key={trip.id} tripObj={trip} onUpdate={getUserTrips} />
+        ))}
+      </div>
+    </div>
+  );
+}
