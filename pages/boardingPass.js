1dbd3ea03cd964eebab022475ff74aa84d45b772
Daun Kim <39635250+yennakim@users.noreply.github.com>
2024-09-13 00:35:27 -0500

    updated import bug

--- pages/boardingPass.js
+++ pages/boardingPass.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { Button } from 'react-bootstrap';
 import { useAuth } from '../utils/context/authContext';
-import BoardingPassCard from '../components/boardingPass/boardingPassCard';
+import BoardingPassCard from '../components/boardingPass/BoardingPassCard';
 import { getBoardingPasses } from '../utils/data/boardingPassData';
 
 export default function BoardingPassPage() {
